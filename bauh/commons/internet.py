b7edf40ce9a406303c027685c75353426d6324ac
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2020-04-07 18:35:49 -0300

    internet off logs

--- bauh/commons/internet.py
+++ bauh/commons/internet.py
@@ -9,8 +9,8 @@ def is_available(client: HttpClient, logger: logging.Logger) -> bool:
     try:
         client.exists('https://google.com', session=False)
         return True
-    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
         if logger:
-            logger.warning('Internet connection seems to be off')
+            logger.warning('Internet connection seems to be off: {}'.format(e.__class__.__name__))
         return False
 
